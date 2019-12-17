09e1f097a70491f2c183e6ead0ed0a561f7990c3
Emanuele Crespan <ecrespan@example.com>
2019-12-17 11:24:13 +0100

    .

--- src/main/java/com/example/git/RobertoPatane.java
+++ src/main/java/com/example/git/RobertoPatane.java
@@ -2,5 +2,14 @@ package com.example.git;
 
 public class RobertoPatane {
 	//sto facendo una modifica sostanzile
-
+	
+	private RobertoPatane instance;
+	
+	private RobertoPatane() {}
+	
+	public RobertoPatane getInstance() {
+		if(instance==null)
+			instance=new RobertoPatane();
+		return instance;
+	}
 }
