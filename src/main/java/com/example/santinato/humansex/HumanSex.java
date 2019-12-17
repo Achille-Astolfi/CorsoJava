91e45f293878def774ec736c1ede4fa05073c4c5
Andrea Santinato <Andrea@umanaforma.it>
2019-12-17 17:36:10 +0100

    Aggiunte nuove Enumeration al package HumanSex

---
+++ src/main/java/com/example/santinato/humansex/HumanSex.java
@@ -0,0 +1,11 @@
+package com.example.santinato.humansex;
+
+public enum HumanSex {
+	// La convenzione usata per le variabili di una enumeration è l'UPPER_SNAIL_CASE
+	// Tutte le costanti che dichiaro sono public static final e del tipo dell'enumeration
+	
+	NOT_KNOW,
+	MALE, 
+	FEMALE,
+	NOT_APPLICABLE;		// L'ULTIMA VIRGOLA E' OPZIONALE
+}
